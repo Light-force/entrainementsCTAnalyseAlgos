dc8abc6c473ee20361385909c45fce053973f7c4
robin <robin.trassard@gmail.com>
2022-12-13 23:01:50 +0100

    modifs

--- rattrapages2022/Exo3/prog_dynamique_question7.py
+++ rattrapages2022/Exo3/prog_dynamique_question7.py
@@ -1,29 +1,23 @@
-n = 9
 T = [90, 150, 130, 50, 60, 140, 70, 80, 120]
 
 
-def ButinMax(n, T):
-    Opt = (n + 1) * [0]
-    Maisons = (n + 1) * [True]
-    Opt[1] = T[0]
-    for i in range(2, n + 1):
-        if Opt[i - 1] < (Opt[i - 2] + T[i - 1]):
-            Opt[i] = Opt[i - 2] + T[i - 1]
+def butinMax(T):
+    maison = []
+    Sol = []
+    for i in range(len(T)):
+        Sol.append(0)
+    for i in range(len(T)):
+        j = 0
+        if (i == 1):
+            Sol[i] = T[i]
         else:
-            Opt[i] = Opt[i - 1]
-            Maisons[i] = False
-    return Opt[n], MaisonsCambriolees(Maisons)
-def MaisonsCambriolees(Maisons):
-    i = len(Maisons) - 1
-    maisonsCambriolees = []
-    while i > 0:
-        if (Maisons[i]):
-            maisonsCambriolees.append(i)
-            i = i - 2
-        else:
-            i = i - 1
-    maisonsCambriolees.sort()
-    return maisonsCambriolees
+            j = i - 1
+            S1 = Sol[j]
+            S2 = Sol[j - 1] + T[i]
+            Sol[i] = max(S1, S2)
+            if max(S1, S2) == S2 and S1 != S2:
+                maison.append(i)
+    return [maison, Sol[len(T) - 1]]
 
 
-print(ButinMax(n, T))
+print(butinMax(T))
